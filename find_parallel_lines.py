a3f3147e9926aaf04ba5180a408440be008d0b91
Sagi Goldfarb <sagi3164@gmail.com>
2021-11-27 01:27:40 +0200

    :(

--- find_parallel_lines.py
+++ find_parallel_lines.py
@@ -19,16 +19,32 @@ def frame_find_lanes(frame):
 
 
 #  Cordinates of the 4 corners of the original source image
-def perspective_trasnform(cropped_img, source_img):
-    height, width =  350,420
+def perspective_trasnform(source_img):
+
+    cropped_img = np.float32([[100,400],[700,400],[0,470],[800,470]])
+    height, width =  70,600
 
     # coordinates of the 4 corners of the target output
-    new_img_coor = np.array([[0,0],[width,0],[0,height],[width,height]])
+    new_img_coor = np.float32([[0,0],[width,0],[0,height],[width,height]])
 
     # use the two sets of four points to compute the perspective transformation matrix
     compute_mat = cv2.getPerspectiveTransform(cropped_img, new_img_coor)
 
-    return cv2.warpPerspective(source_img,compute_mat,(width,height))
+    perspective = cv2.warpPerspective(source_img,compute_mat,(width,height))
+    
+    return perspective
+
+
+# #  Cordinates of the 4 corners of the original source image
+# def perspective_trasnform(cropped_img, source_img):
+#     height, width =  100,850
+#     # coordinates of the 4 corners of the target output
+#     new_img_coor = np.array([[0,0],[width,0],[0,height],[width,height]])
+#     print(new_img_coor)
+#     # use the two sets of four points to compute the perspective transformation matrix
+#     compute_mat = cv2.getPerspectiveTransform(cropped_img, new_img_coor)
+
+#     return cv2.warpPerspective(source_img,compute_mat,(width,height))
 
      
 
--- line_detection.py
+++ line_detection.py
@@ -17,9 +17,21 @@ util.show_image(img,figsize)
 
 cropped_img = fpl.frame_find_lanes(img)
 util.show_image(cropped_img,figsize)
+
 #%%
-p_img = fpl.perspective_trasnform(cropped_img,img)
+cv2.circle(img,(100,400),5,(0,0,250),-1)
+cv2.circle(img,(700,400),5,(0,0,250),-1)
+cv2.circle(img,(0,470),5,(0,0,250),-1)
+cv2.circle(img,(800,470),5,(0,0,250),-1)
+
+util.show_image(img,figsize)
+
+
+#%%
+p_img = fpl.perspective_trasnform(img)
 util.show_image(p_img,figsize)
+# p_img = fpl.perspective_trasnform(cropped_img,img)
+# util.show_image(p_img,figsize)
 
 #%%
 if __name__ == "__main__":
